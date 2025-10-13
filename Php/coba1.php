41e53fb98fed702b047426aa0d7ed3af37540100
Muhammad Fikri <muhammadfikri10050@gmail.com>
2025-10-13 13:10:17 +0700

    fix <br> to /n

--- Php/coba1.php
+++ Php/coba1.php
@@ -5,11 +5,11 @@
     $nilai = 92.75; 
     $status = TRUE; 
 
-    echo  "NIM   : " . $nim . "<br>"; 
-    echo  "Nama  : $nama<br>"; 
+    echo  "NIM   : " . $nim . "/n"; 
+    echo  "Nama  : $nama /n"; 
     print "Umur  : " . $umur; 
     print "<br>"; 
-    printf("Nilai : %.3f<br>", $nilai); 
+    printf("Nilai : %.3f /n", $nilai); 
     if ($status) 
     echo "Status : Aktif"; 
     else 
