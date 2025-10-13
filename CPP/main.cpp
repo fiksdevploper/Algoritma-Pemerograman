720e0038f6d6c26e55d3ef23e9db9a9097df5d2e
Muhammad Fikri <muhammadfikri10050@gmail.com>
2025-10-13 10:44:12 +0700

    upload tugas 2

---
+++ CPP/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string nama = "Muhammad Fikri";
+    int umur = 18;
+    float tinggi = 170.5;
+    bool aktif = true;
+
+    cout << "Nama : " << nama << endl;
+    cout << "Umur : " << umur << " tahun" << endl;  
+    cout << "Tinggi : " << tinggi << " cm" << endl;
+    cout << "Status Aktif: " << aktif << endl;
+    return 0;
+}
