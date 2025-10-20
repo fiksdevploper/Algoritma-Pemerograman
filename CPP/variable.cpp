5b395455684ed920c14457c6ff36756d6789e8b8
Muhammad Fikri <muhammadfikri10050@gmail.com>
2025-10-20 11:08:40 +0700

    upload pembelajaran konstanta

---
+++ CPP/variable.cpp
@@ -0,0 +1,25 @@
+// variable menggunakan cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+   char a[20] = "Belajar Tipe data"; 
+    short b;
+    int c;
+    long d; 
+    float e; 
+    double f;
+    char data_karakter = 'A'; 
+    int data_integer = 10; 
+    float data_real = 3.14; 
+    
+    cout << "Tipe data char = " << a << endl;
+    cout << "Tipe data short = " << b << endl;
+    cout << "Tipe data int = " << c << endl;
+    cout << "Tipe data long = " << d << endl;
+    cout << "Tipe data float = " << e << endl;
+    cout << "Tipe data double = " << f << endl;
+    cout << "Tipe data char = " << data_karakter << endl;
+    cout << "Tipe data integer = " << data_integer << endl;
+    cout << "Tipe data real = " << data_real << endl;
+}
